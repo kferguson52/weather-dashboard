f431a246f989130132d753f177115c851a906945
Kyle <kferguson@captechconsulting.com>
2021-09-16 16:56:46 -0500

    got the data feeding into the UI finally

---
+++ assets/js/homepage.js
@@ -0,0 +1,83 @@
+var APIkey = "a2517b391d3646a20841118e642d9020";
+var searchBtn = document.querySelector("#searchBtn");
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  var userInput = document.getElementById("city").value;
+  weatherAPI(userInput);
+  userInput.value = "";
+}
+
+searchBtn.addEventListener("click", handleFormSubmit);
+function weatherAPI(city) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      oneCall(data);
+    });
+}
+function oneCall(data) {
+  var lat = data.coord.lat;
+  var lon = data.coord.lon;
+  var cityName = data.name;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${APIkey}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      renderCurrentWeather(data.current, cityName);
+      //renderForecast(data.daily)
+    });
+}
+
+function renderCurrentWeather(current, city) {
+  //create variable for data in the current paramente. we need temp, wind, humidity, uvi, date (google converting unix timestamp into date.) icon and how to display icon(hint google open weather icon)
+  var cityName = city;
+  var temp = current.temp;
+  var wind = current.wind_speed;
+  var humid = current.humidity;
+  var uvi = current.uvi;
+  var dateUnix = current.dt;
+  var dateMilSec = dateUnix * 1000;
+  var dateObject = new Date(dateMilSec);
+  var dateString = dateObject.toLocaleString();
+  console.log(current);
+  console.log(temp);
+  console.log(wind);
+  console.log(humid);
+  console.log(uvi);
+  console.log(dateUnix);
+  console.log(dateMilSec);
+  console.log(dateObject);
+  console.log(dateString);
+  console.log(cityName);
+
+  var pDate = document.createElement("h5");
+  pDate.textContent = "Today's Date: " + dateString;
+  document.getElementById("current").appendChild(pDate);
+  var pCity = document.createElement("h5");
+  pCity.textContent = "City: " + cityName;
+  document.getElementById("current").appendChild(pCity);
+  var pTemp = document.createElement("h5");
+  pTemp.textContent = "Temperature: " + temp;
+  document.getElementById("current").appendChild(pTemp);
+  var pWind = document.createElement("h5");
+  pWind.textContent = "Wind Speed (mph) : " + wind;
+  document.getElementById("current").appendChild(pWind);
+  var pHum = document.createElement("h5");
+  pHum.textContent = "Humidity: " + humid;
+  document.getElementById("current").appendChild(pHum);
+
+ 
+  //create elements that we need to displau
+  //set textContext in those elements
+  //append all created elements in the appropriate container.
+}
+
+// After getting the lat and long values from the geo API store in variable, we are gonna use these values to call the One Call Api
+// Write a function that calls the One Call API to retreive the info for the forecast
+// Populates the div blocks with the upcoming 5 day forecast current weather info
